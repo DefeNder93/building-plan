2ccddf58335c3e1b1e1d175a3f59fd3852ba5e10
DefeNder93 <alexdefender93@gmail.com>
2017-05-07 17:50:06 +0300

    floors, buildings

--- app/buildingInfo/buildingInfo.ctrl.js
+++ app/buildingInfo/buildingInfo.ctrl.js
@@ -1,8 +1,22 @@
 app.controller("buildingInfo", function($scope, Api) {
+    $scope.buildings = [];
+    $scope.floors = [];
+    $scope.active = {
+        building: null,
+        floor: null
+    };
     Api.getBuildings().then(function(r){
-        console.log('Buildings');
-        console.log(r.data);
+        $scope.buildings = r.data;
+        $scope.chooseBuilding($scope.buildings[0]);
     }).catch(function(){
         console.log('error');
     });
+    $scope.chooseBuilding = function(building) {
+        $scope.active.building = building;
+        $scope.active.floor = building.floors[0];
+        $scope.floors = building.floors;
+    };
+    $scope.chooseFloor = function(floor) {
+        $scope.active.floor = floor;
+    }
 });
--- app/editImage/imageMap.dir.js
+++ app/editImage/imageMap.dir.js
@@ -1,14 +1,27 @@
 app.directive('imageMap', function ($http) {
     return {
         restrict: 'E',
+        scope: {
+            imageLink: '='
+        },
         link: function(scope, el, attrs) {
-            var draw = SVG('image-map').size(1200, 500);
-            draw.attr('class', 'scaling-svg');
-            draw.attr('viewBox', '0 0 1898.1851 1601.6219');
+            var draw;
 
-            $http.get('img/floor1.svg').then(function(r){
-                draw.svg(r.data);
+            scope.$watch('imageLink',function(link){
+                if (link) {
+                    $http.get(link).then(function(r){
+                        createSvg(r.data);
+                    });
+                }
             });
+
+            function createSvg(data) {
+                draw && (draw.remove());
+                draw = SVG('image-map').size(1200, 500);
+                draw.attr('class', 'scaling-svg');
+                draw.attr('viewBox', '0 0 1898.1851 1601.6219');
+                draw.svg(data);
+            }
         }
     };
 });
